c8edc147ed2690b1dfc47edcb046a63da9a08769
GasinskiN <gasinskinorman@gmail.com>
2023-03-11 19:16:33 +0100

    Put everything in an async function and add closing function

--- app.js
+++ app.js
@@ -5,66 +5,65 @@ main().catch(err => console.log(err));
     
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB');
-}
-
-const fruitSchema = new mongoose.Schema ({
-    name: String,
-    rating: Number,
-    review: String
-});
-
-const Fruit = mongoose.model("Fruit", fruitSchema);
-
-const apple = new Fruit ({
-    name: "Apple",
-    rating: 7,
-    review: "A pretty good fuit."
-});
-
-// apple.save();
-
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-});
-
-const Person = mongoose.model("Person", personSchema);
-
-const joe = new Person({
-    name: "Joe",
-    age: 62
-});
-
-// joe.save();
-
-
-const pear = new Fruit({
-    name: "Pear",
-    rating: 8,
-    review: "A scrumptious and sweet fruit"
-});
-
-const pineapple = new Fruit({
-    name: "Pineapple",
-    rating: 7,
-    review: "A good tasting fruit but the preparing process is a pain"
-});
-
-const strawberry = new Fruit({
-    name: "Strawberry",
-    rating: 8,
-    review: "If you can find ripe ones they are great"
-});
-
-
-run();
-
-async function run(){
+    const fruitSchema = new mongoose.Schema ({
+        name: String,
+        rating: Number,
+        review: String
+    });
+    
+    const Fruit = mongoose.model("Fruit", fruitSchema);
+    
+    const apple = new Fruit ({
+        name: "Apple",
+        rating: 7,
+        review: "A pretty good fuit."
+    });
+    
+    // apple.save();
+    
+    
+    const personSchema = new mongoose.Schema({
+        name: String,
+        age: Number,
+    });
+    
+    const Person = mongoose.model("Person", personSchema);
+    
+    const joe = new Person({
+        name: "Joe",
+        age: 62
+    });
+    
+    // joe.save();
+    
+    
+    const pear = new Fruit({
+        name: "Pear",
+        rating: 8,
+        review: "A scrumptious and sweet fruit"
+    });
+    
+    const pineapple = new Fruit({
+        name: "Pineapple",
+        rating: 7,
+        review: "A good tasting fruit but the preparing process is a pain"
+    });
+    
+    const strawberry = new Fruit({
+        name: "Strawberry",
+        rating: 8,
+        review: "If you can find ripe ones they are great"
+    });
+    
+    // Fruit.insertMany([pear, pineapple, strawberry])
+    //     .then(() => console.log("Inputting the items was succesful"))
+    //     .catch((err) => console.log(err))
+    
     try{
         const myfruit = await Fruit.find();
-        console.log(myfruit);
+        myfruit.forEach(el => console.log(el.name));
     } catch(err){
         console.log(err);
     }
+    mongoose.connection.close();
 }
