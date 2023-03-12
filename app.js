0224c0993a49e4ec90c8a9fb8fd2ac950c3f5a86
GasinskiN <gasinskinorman@gmail.com>
2023-03-12 16:20:57 +0100

    Add updateMany and deleteMany functionality, add comments and rearrange the code a little bit

--- app.js
+++ app.js
@@ -1,76 +1,80 @@
 const mongoose = require('mongoose');
-    
-const fruitSchema = new mongoose.Schema ({
-    name: {
-        type: String,
-        required: [true, "Why no name"]
-    },
-    rating: Number,
-    review: String
-});
+const Fruit = require("./Fruit"); 
+const Person = require("./Person");  
 
-const Fruit = mongoose.model("Fruit", fruitSchema);
 
 //connect to MongoDB by specifying port to access MongoDB server
 main().catch(err => console.log(err));
+
+// make some fruit objects for testing insertMany functionality
+const pear = new Fruit({
+    name: "Pear",
+    rating: 8,
+    review: "A scrumptious and sweet fruit"
+});
+
+const pineapple = new Fruit({
+    name: "Pineapple",
+    rating: 7,
+    review: "A good tasting fruit but the preparing process is a pain"
+});
+
+const strawberry = new Fruit({
+    name: "Strawberry",
+    rating: 8,
+    review: "If you can find ripe ones they are great"
+});
+
     
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB');
     
+    // try{
+    //     const apple = await Fruit.create({
+    //         name: "Joel",
+    //         rating: 5,
+    //         review: "its ok"
+    //     });
+    // } catch (err) {
+    //     console.log(err.message);
+    // }
     
     
-    try{
-        const apple = await Fruit.create({
-            rating: 5,
-            review: "its ok"
-        });
-    } catch (err) {
-        console.log(err);
-    }
+    // const joe = new Person({
+    //     name: "Joe",
+    //     age: 62
+    // });
+    
+    // await joe.save();
     
     
-    const personSchema = new mongoose.Schema({
-        name: String,
-        age: Number,
-    });
-    
-    const Person = mongoose.model("Person", personSchema);
-    
-    const joe = new Person({
-        name: "Joe",
-        age: 62
-    });
-    
-    // joe.save();
-    
-    
-    const pear = new Fruit({
-        name: "Pear",
-        rating: 8,
-        review: "A scrumptious and sweet fruit"
-    });
-    
-    const pineapple = new Fruit({
-        name: "Pineapple",
-        rating: 7,
-        review: "A good tasting fruit but the preparing process is a pain"
-    });
-    
-    const strawberry = new Fruit({
-        name: "Strawberry",
-        rating: 8,
-        review: "If you can find ripe ones they are great"
-    });
+
     
     // Fruit.insertMany([pear, pineapple, strawberry])
     //     .then(() => console.log("Inputting the items was succesful"))
     //     .catch((err) => console.log(err))
     
+    // try {
+    //     await Fruit.insertMany([pear, pineapple, strawberry]);
+    //     console.log("Insert many operation was succesfull");
+        
+    // } catch (err) {
+    //     console.log(err.message);
+    // }
+
+
     try{
         const myfruit = await Fruit.find();
         console.log(myfruit);
     } catch(err){
-        console.log(err);
+        console.log(err.message);
     }
+
+    // const res = await Fruit.updateMany({ rating: 5 }, { name: "Orange"});
+    // console.log([res.acknowledged, res.modifiedCount]);
+
+    // const response = await Fruit.deleteMany({ name: "Apple"});
+    // console.log(response.deletedCount);
+
     mongoose.connection.close();
 }
