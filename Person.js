5e9e68dd1b784a57a8edc793c454401d35b33c18
GasinskiN <gasinskinorman@gmail.com>
2023-03-12 16:34:53 +0100

    Add a date with a default and immutable parameters, add number validation to age in the personSchema

--- Person.js
+++ Person.js
@@ -3,7 +3,16 @@ const mongoose = require("mongoose");
 
 const personSchema = new mongoose.Schema({
     name: String,
-    age: Number,
+    age: {
+        type: Number,
+        min: [18, "Only for people over the age of 18"],
+        max: [125, "Your age is higher than the longest living person you should apply for a guiness record"]
+    },
+    createdAt: {
+        type: Date,
+        immutable: true,
+        default: () => Date.now(),
+    }
 });
 
 module.exports = mongoose.model("Person", personSchema);
